686009727ca44b856ef64cbf8a7b8bb4784c879b
opesh <temitayoope3@gmail.com>
2022-10-14 15:22:24 -0700

    merge

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry
+ * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
 	int i;
-	char j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
-	}
-	for (j = 'a'; j <= 'f'; j++)
-	{
-		putchar(j);
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
