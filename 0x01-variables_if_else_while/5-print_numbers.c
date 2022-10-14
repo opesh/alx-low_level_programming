7a069a2af9fce9b2bf927d5fd93e98d9ebf612e8
opesh <temitayoope3@gmail.com>
2022-10-14 13:35:20 -0700

    merge

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
