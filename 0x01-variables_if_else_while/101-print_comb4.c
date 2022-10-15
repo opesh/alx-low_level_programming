4f5fca3cd2f93717caeb570c413e025c63bfac39
opesh <temitayoope3@gmail.com>
2022-10-15 04:08:25 -0700

    merge

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = x + 1; y < 9; y++)
+		{
+			for (z = y + 1; z < 10; z++)
+			{
+				putchar((x % 10) + '0');
+				putchar((y % 10) + '0');
+				if (x == 7 && y == 8 && z == 9)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
