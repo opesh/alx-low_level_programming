e426fd5e6ac7b88123262700ebec65909a449008
opesh <temitayoope3@gmail.com>
2022-10-18 15:54:01 -0700

    merge

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,8 +26,8 @@ void times_table(void)
 				}
 				if (b < 9)
 				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar (',');
+					_putchar (' ');
 				}
 			}
 			else
@@ -36,8 +36,8 @@ void times_table(void)
 				_putchar ((c % 10) + '0');
 				if (b < 9)
 				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar (',');
+					_putchar (' ');
 				}
 			}
 		}
