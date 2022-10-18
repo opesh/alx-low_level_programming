ed5338c99a5c94dc30910567004b47f3ba4b73a6
opesh <temitayoope3@gmail.com>
2022-10-18 12:55:10 -0700

    merge

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,6 +14,7 @@ void jack_bauer(void)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
+			_putchar(':');
 			_putchar((M / 10) + '0');
 			_putchar((M % 10) + '0');
 			_putchar('\n');
