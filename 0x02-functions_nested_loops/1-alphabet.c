961a6c454516ba4ebd1f6228c7b667ed2a216a5a
opesh <temitayoope3@gmail.com>
2022-10-18 06:05:24 -0700

    merge

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ *print_alphabet - a function that prints the alphabet, in lowercase
+ *followed by a new line
  * Return: Always 0.
  */
 void print_alphabet(void)
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - a function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ * Return: Nothing
  */
 void print_alphabet_x10(void)
 {
