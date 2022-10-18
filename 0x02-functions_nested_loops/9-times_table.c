5e12ecdc76cc13c6a6b6d4975c433cbb090b87e6
opesh <temitayoope3@gmail.com>
2022-10-18 13:43:43 -0700

    merge

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	for (row = 0; row <= 9; row++)
 	{
 		_putchar(0);
-		for (column = 1; column <= 9; column++)
+		for (column = 0; column <= 9; column++)
 		{
 			_putchar(',');
 			_putchar(' ');
