c0dbf853fee918f87210082d29ef05f11af5d434
opesh <temitayoope3@gmail.com>
2022-10-18 13:58:29 -0700

    merge

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 				_putchar(' ');
 			}
 			product = row * column;
-			if (product <= 9)
+			if (product > 0 && product <= 9)
 			{
 				_putchar(' ');
 				_putchar((product % 10) + '0');
