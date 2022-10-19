75b7b0e178ac2cbb155e7dab1d30f444ee0f3feb
opesh <temitayoope3@gmail.com>
2022-10-18 17:12:30 -0700

    merge

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,37 +10,22 @@ void times_table(void)
 	int c;
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar(0);
+		for (b = 1; b <= 9; b++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			c = a * b;
-			if ((c / 10) == 0)
+			if (c <= 9)
 			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar (',');
-					_putchar (' ');
-				}
+				_putchar(' ');
 			}
 			else
 			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar (',');
-					_putchar (' ');
-				}
+				_putchar((c / 10) + '0');
 			}
+			_putchar((c % 10) + '0');
 		}
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }
