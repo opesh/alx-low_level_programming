b94f0b9ec38c297dd47edb2829119b6eb62f459b
opesh <temitayoope3@gmail.com>
2022-10-20 16:14:56 -0700

    merge

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,15 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (i == 1)
 		{
