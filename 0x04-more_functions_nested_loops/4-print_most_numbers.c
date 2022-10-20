016827456e7ddb9f10a7efea69a5745c339f55ea
opesh <temitayoope3@gmail.com>
2022-10-20 06:27:42 -0700

    merge

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9.
+ *
+ * Return: nothing.
+ */
+
+void print_most_numbers(void)
+{
+	n = 0;
+
+	while (n < 10)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+		n++;
+	}
+	_putchar('\n');
+}
