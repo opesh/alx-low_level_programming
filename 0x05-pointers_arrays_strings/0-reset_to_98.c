2441917121530492ed2621359c63b225e1258c6e
opesh <temitayoope3@gmail.com>
2022-10-24 12:44:21 -0700

    merge

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * the value it points to 98
+ * @n: pointer to an int
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
