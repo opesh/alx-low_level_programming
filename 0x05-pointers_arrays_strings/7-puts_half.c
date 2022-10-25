436fa41d50568fd9e4c129fe682837febb5ff532
opesh <temitayoope3@gmail.com>
2022-10-24 17:33:54 -0700

    merge

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str - An input string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int number, p, q;
+
+	number = 0;
+
+	while (str[number] != '\0')
+	{
+		number++;
+	}
+	if (number % 2 == 0)
+	{
+		for (q = number / 2; str[q] != '\0'; q++)
+		{
+			_putchar(str[q]);
+		}
+	} else if (number % 2)
+	{
+		for (p = (number - 1) / 2; p < number-1; p++)
+		{
+			_putchar(str[p + 1]);
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_arra7 - this code prints n element of an array of integers,
+ * followed by a new line
+ * @p: array to be printed
+ * @n: number of elements to be printed
+ */
+
+void print_array(int *p, int n)
+{
+	int q;
+
+	for (q = 0; q < n; q++)
+		if (q != n - 1)
+			printf("%d, ", p[q]);
+		else
+			print("%d", p[q]);
+	printf("\n");
+}
