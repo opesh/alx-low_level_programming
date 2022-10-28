55c04b2870e0f98e458ae5bea49292074ea3dc8f
opesh <temitayoope3@gmail.com>
2022-10-28 13:47:57 -0700

    merge

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * _strncat - A function that concatenates two strings
+ * @dest: string to be copied to
+ * @src: string to copy
+ * @n: No of btyes to copy
+ * Return: the destination string (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
