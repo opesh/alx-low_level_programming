996d85b6295c551a817a48cb5c85e536ceb93675
opesh <temitayoope3@gmail.com>
2022-10-28 17:18:10 -0700

    merge

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - this code capitalizes every word of a string
+ * @s: string to modify
+ * Return: resulting string
+ */
+
+char *cap_string(char *s)
+{
+	int p, q;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', };
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		if (p == 0 && s[p] >= 'a' && s[p] <= 'z')
+			s[p] -= 32;
+
+		for (q = 0; q < 13; q++)
+		{
+			if (s[p] == spe[q])
+			{
+				if (s[p + 1] >= 'a' && s[p + 1] <= 'z')
+				{
+					s[p + 1] -=32;
+				}
+			}
+		}
+	}
+	return (s);
+}
