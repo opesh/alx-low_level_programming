21a1764b9c655fdded9538156700bda747a5848d
opesh <temitayoope3@gmail.com>
2022-11-02 07:51:41 -0700

    merge

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recurssion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+
 }
