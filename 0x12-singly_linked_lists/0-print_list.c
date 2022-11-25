ca1d0b13dc1233b17920641b5a1438b01f250316
opesh <temitayoope3@gmail.com>
2022-11-25 14:04:11 -0800

    merge

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[0] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
