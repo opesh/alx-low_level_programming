084aeaff1b312f25811879d6bb0c6ccec403c3dd
opesh <temitayoope3@gmail.com>
2022-11-28 12:42:25 -0800

    merge

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * print_listint - prints all the elements of a linked list
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef LIST_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdio.h>
